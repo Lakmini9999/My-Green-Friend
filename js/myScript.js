6d89bfaa41a83317b6c2c5e2155da2f32c3f5c39
Lakmini9999 <manthrirathna.mal@gmail.com>
2019-10-11 15:00:38 +0530

    Signed-off-by: Lakmini9999 <manthrirathna.mal@gmail.com>

--- js/myScript.js
+++ js/myScript.js
@@ -1,55 +1,12 @@
-$(document).ready(function () {
-    /*    $('.ryu').mouseenter(function(){
-            $('.ryu-still').hide();
-            $('.ryu-ready').show();
-        })
-        .mouseleave(function() {
-            $('.ryu-still').show();
-            $('.ryu-ready').hide();
-        })
-        .mousedown(function(){
-            playHadouken();
-            $('.ryu-ready').hide();
-            $('.ryu-throwing').show();
-            $('.hadouken').finish().show().animate(
-                {'left': '1020px'},
-                1500,
-                function(){
-                    $('.hadouken').hide();
-                    $('.hadouken').css('left', '520px');
-                });
-        })
-        .mouseup(function(){
-            $('.ryu-throwing').hide();
-            $('.ryu-ready').show();
-        });*/
-
-     $("#carouselExampleIndicators").carousel();
-
-  // Enable Carousel Indicators
-  $(".carousel-item").click(function(){
-    $("#carouselExampleIndicators").carousel(0);
-  });
-  $(".carousel-item").click(function(){
-    $("#carouselExampleIndicators").carousel(1);
-  });
-  $(".carousel-item").click(function(){
-    $("#carouselExampleIndicators").carousel(2);
-  });
-
-  // Enable Carousel Controls
-  $(".left").click(function(){
-    $("#carouselExampleIndicators").carousel("prev");
-  });
-  $(".right").click(function(){
-    $("#carouselExampleIndicators").carousel("next");
-  });
-
-
+$(document).ready(function() {
+    playH();
 });
 
-function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
+function playH() {
+    $('#my_audio')[0].volume = 0.5;
+    $('#my_audio')[0].load();
+    $('#my_audio')[0].play().catch(function() {
+    // do something
+});
+    alert("Dear Visitor, Welcome To My Green Friend!! It's nice to have you hear");
 }
